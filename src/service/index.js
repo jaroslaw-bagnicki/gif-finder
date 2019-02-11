e02a40b6b2e2ae7db5a471947f6b55ccdb269aee
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-11 20:07:48 +0100

    Add service mock

---
+++ src/service/index.js
@@ -0,0 +1,7 @@
+export function getGif(searchText) {
+  console.log('getGif()');
+  return Promise.resolve({
+    giphyUrl: 'https://gph.is/1LjlEFE',
+    gifUrl: 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif'
+  });
+}
