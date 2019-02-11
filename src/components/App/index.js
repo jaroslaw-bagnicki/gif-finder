ad38afe18a853fd4336bb9be9b14f7f43dfe8f1a
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-11 20:04:23 +0100

    Finish App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import styles from './styles.module.scss';
+import * as service from '../../service';
 
 import { Search, Gif } from '../';
 
 export class App extends Component {
+  state = {
+    isActive: false,
+    isLoading: false,
+    searchText: '',
+    gifUrl: '',
+    giphyUrl: ''
+  }
+
   render() {
+    const { isActive, isLoading, gifUrl, giphyUrl } = this.state;
     return (
       <div className={styles.container}>
         <h1>GIFs Finder!</h1>
         <p>Find gif on <a href="http://giphy.com">giphy</a>. Click enter to search </p>
-        <Search />
-        <Gif />
+        <Search onSearch={this.handleSearch} toggleIsActive={this.toggleIsActive} />
+        <Gif 
+          isActive={isActive}
+          isLoading={isLoading}
+          gifUrl={gifUrl}
+          giphyUrl={giphyUrl} />
       </div>
     );
+  }
+
+  handleSearch = (searchText) => {
+    this.setState({
+      isActive: true,
+      isLoading: true
+    });
+    service.getGif(searchText)
+      .then( res => {
+        this.setState({
+          gifUrl: res.gifUrl,
+          giphyUrl: res.giphyUrl,
+          isLoading: false
+        });
+      });
+  }
+
+  toggleIsActive = () => {
+    this.setState({
+      isActive: false
+    });
   }
 }
