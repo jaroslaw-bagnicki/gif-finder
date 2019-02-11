6531db84025ae93404cfb0aa347bc25c8d4d7457
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-11 19:14:25 +0100

    WIP: Gif component

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
 export class Gif extends Component {
+
+  static propTypes = {
+    isStarted: PropTypes.bool,
+    isLoading: PropTypes.bool,
+    giphyUrl: PropTypes.string,
+    gifUrl: PropTypes.string
+  }
+
   render() {
+    const { isStarted, isLoading, giphyUrl, gifUrl } = this.props;
     return (
-      <div> -- Gif component --</div>
+      isStarted && (
+        <div className={styles.container}>
+          { isLoading ? 
+            '-- Loader --' 
+            : 
+            <a href={giphyUrl} title="Open in Giphy" target="new">
+              <img src={gifUrl} className={styles.gifImg} />
+            </a>
+          }
+        </div>
+      )
     );
   }
 }
