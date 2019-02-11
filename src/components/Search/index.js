e32577ec09c43ffce17fbbad9e78193f78dc3f02
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-11 20:04:58 +0100

    Finish Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
 export class Search extends Component {
+  static propTypes = {
+    onSearch: PropTypes.func.isRequired,
+    toggleIsActive: PropTypes.func.isRequired
+  }
+
   state = {
     searchText: ''
   };
@@ -13,6 +19,7 @@ export class Search extends Component {
           className={styles.searchInput}
           type="text"
           onChange={this.handleChange}
+          onKeyUp={this.handleEnterPress}
           value={this.state.searchText}
         />
       </div>
@@ -20,8 +27,14 @@ export class Search extends Component {
   }
 
   handleChange = (e) => {
+    const searchText = e.target.value;
+    if (searchText === '') this.props.toggleIsActive();
     this.setState({
-      searchText: e.target.value
+      searchText
     });
+  }
+
+  handleEnterPress = (e) => {
+    if (e.keyCode === 13) this.props.onSearch(this.state.searchText);
   }
 }
